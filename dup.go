339d5654ae2895feda663627568cebcca9dd7bc0
Daniil <danast_2@mail.ru>
2024-05-06 07:34:14 +0300

    06.04.2024/07:34

--- dup.go
+++ dup.go
@@ -13,9 +13,13 @@ func main() {
 
 	counts := make(map[string]int, 6) // 6 - емкость мапы, Это не означает, что карта будет ограничена ровно 6 элементами,
 	// просто это предоставляет Go информацию о начальном размере карты, чтобы избежать динамического изменения её размера, если это возможно.
+
 	input := bufio.NewScanner(os.Stdin)
+	//var input *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
 	fmt.Printf("Type: %T\n", counts) //%T формат вывода для типа данных переменной
 	fmt.Printf("Type: %T\n", input)
+
 	for i := 0; i < 6 && input.Scan(); i++ {
 		counts[input.Text()]++
 	}
