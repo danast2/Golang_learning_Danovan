caf5700d0f085984cb79e2a22abf741e70b81888
Daniil <danast_2@mail.ru>
2024-05-06 12:54:13 +0300

    06.04.2024/12:54

--- duup2.go
+++ duup2.go
@@ -19,10 +19,17 @@ func main() {
 				continue
 			}
 			countLines(f, counts)
-			f.Close()
+			err = f.Close()
+			if err != nil {
+				return
+			}
 		}
 	}
-
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
 
 func countLines(f *os.File, counts map[string]int) {
