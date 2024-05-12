15c4ef23d7246a9fcdacaed84d1cad6d3efcbffc
Daniil <danast_2@mail.ru>
2024-05-12 11:24:04 +0300

    12.05.2024/11:24

--- fetch_new_two.go
+++ fetch_new_two.go
@@ -27,7 +27,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		//fmt.Printf("%s\n", b)
+		fmt.Println("Статус ответа:", resp.Status) //статус ответа
 	}
 }
 
