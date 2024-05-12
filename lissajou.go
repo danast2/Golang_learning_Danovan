6119ba83dd04e5f5187d85d885f4a8e5b4693158
Daniil <danast_2@mail.ru>
2024-05-12 09:31:38 +0300

    12.05.2024/09:31

--- lissajou.go
+++ lissajou.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.RGBA{9, 176, 9, 1}, color.Black}
+var palette = []color.Color{color.RGBA{0, 255, 0, 255}, color.Black}
 
 const (
 	greenIndex = 0 // первый цвет палитры
@@ -35,13 +35,23 @@ func lissajou(out io.Writer) {
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0
 	for i := 0; i < nframes; i++ {
+
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
+
+		// Устанавливаем черный цвет фона
+		for y := 0; y < 2*size+1; y++ {
+			for x := 0; x < 2*size+1; x++ {
+				img.SetColorIndex(x, y, blackIndex)
+			}
+		}
+
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
 		}
+
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
 		anim.Image = append(anim.Image, img)
