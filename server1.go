c8a089a17a2512a8eb711bb943c713020bda31aa
Daniil <danast_2@mail.ru>
2024-05-14 09:05:56 +0300

    14.05.2024/09:05

--- server1.go
+++ server1.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handler) // каждый запрос вызывает обработчик
+	http.HandleFunc("/", handlerFunc) // каждый запрос вызывает обработчик
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
---
+++ server2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+}
+
+// обработчик, возвращающий компанент пути запрашиваемого URL
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+// счетчик, возврашающий кол-во сделанных запросов
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
